e4139e97d295699027351fccca6928303495a858
DmytroYablonovskyi <yablonovskydmutro1@gmail.com>
2025-10-28 16:56:43 +0200

    Added blank market overview page, added to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dasboard/dashboard.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { OverviewComponent } from './pages/marketOverview/marketOverview.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'overview', component: OverviewComponent },
 ];
