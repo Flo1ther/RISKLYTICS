203ae61951609a8865d2cb0c149392a1fc33cb06
DmytroYablonovskyi <yablonovskydmutro1@gmail.com>
2025-10-29 18:18:33 +0200

    Added about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'about', component: AboutComponent },
 ];
