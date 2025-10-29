f362ed9f8e3b7a88577bf3c94a55ff5fc5f3c055
DmytroYablonovskyi <yablonovskydmutro1@gmail.com>
2025-10-29 18:08:20 +0200

    Added example for compare assets page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CompareComponent } from './pages/compareAssets/compare.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'compare', component: CompareComponent },
 ];
